1ac1fbd4491a0ce4149f5c62b874037e3d9d31b5
Paul LaMotte <pmlamotte@gmail.com>
2015-02-07 20:52:24 -0600

    Updated gulp to watch for new files

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,45 @@
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	to5 = require('gulp-6to5'),
-	plumber = require('gulp-plumber');
- 
-gulp.task('serve', ['build', 'watch', 'connect'])
+	plumber = require('gulp-plumber'),
+	watch = require('gulp-watch');
+
+gulp.task('serve', ['build', 'connect'])
 
 gulp.task('connect', function() {
 	connect.server({
-	    root: ['src', '.'],
+	    root: ['public', '.'],
 	    port: 8080,
 	    livereload: true
 	});
 });
 
-gulp.task('build', function() {
-	return gulp.src('src/scripts/**/*.js')
-		.pipe(plumber())
-        .pipe(to5())
-        .pipe(gulp.dest('public/scripts'));
-})
+gulp.task('build', ['6to5', 'bowerCopy', 'htmlCopy', 'imageCopy']);
 
-gulp.task('watch', function() {
-	gulp.watch('src/scripts/**/*.js', ['build']);
+gulp.task('6to5', function() {
+	return gulp.src('src/scripts/**/*.js')
+				.pipe(watch('src/scripts/**/*.js'))
+				.pipe(plumber())
+		        .pipe(to5())
+		        .pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('bowerCopy', function() {
+	return gulp.src('bower_components/**/*')
+				.pipe(watch('bower_components/**/*'))
+				.pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('htmlCopy', function() {
+	return gulp.src(['src/**/*.html'])
+				.pipe(watch('src/**/*.html'))
+				.pipe(gulp.dest('public/'));
+});
+
+gulp.task('imageCopy', function() {
+	return gulp.src(['src/images/**/*'])
+				.pipe(watch('src/images/**/*'))
+				.pipe(gulp.dest('public/images'));
 })
  
 gulp.task('default', ['serve']);
