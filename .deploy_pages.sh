2c059af59e2f15c2728bab9ae8159cf396fd0436
Paul LaMotte <pmlamotte@gmail.com>
2015-02-13 19:28:58 -0600

    Initial setup of travis build

---
+++ .deploy_pages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf out || exit 0;
+mkdir out;
+
+GH_REPO="@github.com/pmlamotte/Heister.git"
+
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+for files in '*.tar.gz'; do
+        tar xfz $files
+done
+
+cd out
+git clone $FULL_REPO .
+git checkout gh-pages
+git config user.name "heister-travis"
+git config user.email "travis"
+
+git rm -r *
+cp -R ../public/* .
+
+git add .
+git commit -m "deployed to github pages"
+git push --quiet $FULL_REPO gh-pages
